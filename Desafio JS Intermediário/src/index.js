ca7b3bca4d1373a242611b60ded3ec8b80ca7425
Adrian Salvioni <138667583+Speaknoway@users.noreply.github.com>
2024-07-03 09:53:50 +0400

    Correção de código JS
    Corrigido o código JS para não enviar o arquivo sem as informações adequadas

--- Desafio JS Intermediário/src/index.js
+++ Desafio JS Intermediário/src/index.js
@@ -1,20 +1,23 @@
 const caixaDeTexto = document.querySelectorAll(".dadosDoCliente");
-const enviarForm = document.getElementById("enviarForm");
+const enviarForm = document.getElementById("formulario");
 console.log(caixaDeTexto)
+const aviso = document.querySelectorAll(".aviso")
+
+console.log(aviso)
 
 
-
-enviarForm.addEventListener("click", function(event) {
+enviarForm.addEventListener("submit", function(event) {
     event.preventDefault();
 
     caixaDeTexto.forEach(informacoes =>{
         if(informacoes.value === ""){
-            informacoes.classList.add("nao-preenchido");
-            informacoes.classList.remove("esconder")
+            informacoes.classList.add("nao-preenchido");    
         }else{
             informacoes.classList.add("preenchido");
+            informacoes.classList.remove("nao-preenchido");
         }
     })
     
 
 });
+
