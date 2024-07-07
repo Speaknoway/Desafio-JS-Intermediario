23f7d3716264faf22898665e6b3d3e43a857d874
Adrian Salvioni <138667583+Speaknoway@users.noreply.github.com>
2024-07-07 07:58:22 +0400

    Add files via upload
    Correção de erros e alteração necessária para o código funcionar da forma adequada.

--- Desafio JS Intermediário/src/index.js
+++ Desafio JS Intermediário/src/index.js
@@ -1,23 +1,23 @@
+
 const caixaDeTexto = document.querySelectorAll(".dadosDoCliente");
 const enviarForm = document.getElementById("formulario");
-console.log(caixaDeTexto)
-const aviso = document.querySelectorAll(".aviso")
-
-console.log(aviso)
+const esconder = document.querySelectorAll(".esconder")
 
 
 enviarForm.addEventListener("submit", function(event) {
     event.preventDefault();
 
     caixaDeTexto.forEach(informacoes =>{
+        const aviso = informacoes.nextElementSibling;
+        
         if(informacoes.value === ""){
-            informacoes.classList.add("nao-preenchido");    
+            informacoes.classList.add("nao-preenchido"); 
+            aviso.classList.remove("esconder");
+            
         }else{
             informacoes.classList.add("preenchido");
             informacoes.classList.remove("nao-preenchido");
+            aviso.classList.add("esconder");
         }
     })
-    
-
 });
-
